22d9c05352dea019ec679ff1a16ac21f3af46e31
Erik Vaněk <erikvanek0@gmail.com>
2024-12-03 23:10:00 +0100

    Fixed assiging constants num 4

--- www/config.php
+++ www/config.php
@@ -30,4 +30,6 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-define('BASE_URL', '');
+if (!defined('BASE_URL')) {
+    define('BASE_URL', '');
+}
