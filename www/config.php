2c7d7e65a6e06757acc00ab87bf88ad9acbd23bd
Erik Vaněk <erikvanek0@gmail.com>
2024-12-03 23:05:43 +0100

    Fixed assiging constants

--- www/config.php
+++ www/config.php
@@ -2,14 +2,6 @@
 
 const ROOT = __DIR__ . '/../';
 const VIEWS = ROOT . 'src/Views/';
-const BASE_URL = '/~vanekeri/';
-
-
-// Get local config
-if (file_exists(ROOT . 'config.local.php')) {
-    require ROOT . 'config.local.php';
-    die();
-}
 
 $_ENV['database'] = [
     'server' => 'localhost',
@@ -18,15 +10,26 @@ $_ENV['database'] = [
     'password' => 'petrpaveluwu',
 ];
 
-
-// Set error reporting
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 // Set upload files permissions
 ini_set('file_uploads', '1');
 ini_set('upload_max_filesize', '1M');  // Max file size for uploads
 ini_set('post_max_size', '1M');        // Max POST data size
 ini_set('max_execution_time', '30');    // Max script execution time in seconds
 ini_set('max_input_time', '60');
+
+// Load local configuration
+if (file_exists(ROOT . 'config.local.php')) {
+    require ROOT . 'config.local.php';
+    die();
+}
+
+
+
+// Set error reporting
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (!BASE_URL !== null) {
+    define("BASE_URL", '');
+}
