71b0389649c39ded7cef400220fd0024e4e2c3df
Erik Vaněk <erikvanek0@gmail.com>
2024-12-05 21:31:16 +0100

    Added displaying data on admin site, overlay with content details window

--- src/Views/admin.php
+++ src/Views/admin.php
@@ -1,36 +1,100 @@
+<?php
+
+use Helpers\UrlHelper;
+use Logic\Router;
+
+if (!isset($_SESSION['user_data'])) {
+    Router::redirect(path: 'user', query: ['error' => 'loggedOut']);
+}
+
+if (!$_SESSION['user_data']->isAdmin()) {
+    Router::redirect(path: 'user', query: ['error' => 'noPrivileges']);
+}
+
+?>
+
 <main>
-    <section>
-        <h2>Administrace uživatelů</h2>
-        <table class="users-table">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Uživatelské jméno</th>
-                <th>Celé jméno</th>
-                <th>Role</th>
-                <th>Datum registrace</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
+    <div class="tables">
+        <h1>Administrace</h1>
+        <p id="warning-display">Tato stránka není podporovaná v portrétním režimu, zkuste otočtit prosím zařízení na šířku, nebo použijte počítač.</p>
+        <section class="table-data">
+            <h2>Uživatelé</h2>
+            <table class="users-table">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Uživatelské jméno</th>
+                    <th>Celé jméno</th>
+                    <th>Role</th>
+                    <th>Datum registrace</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
                 if (empty($users)) {
                     echo '<tr><td colspan="5">Žádní uživatelé nenalezeni</td></tr>';
                 } else {
                     foreach ($users as $user) {
-                        echo '<tr>
-                                <td>'. htmlspecialchars($user->getId()) .'</td>
+                        echo '<tr id="user-'. $user->getId() .'">
+                                <td>'. $user->getId() .'</td>
                                 <td>'. htmlspecialchars($user->getUsername()) .'</td>
                                 <td>'. htmlspecialchars($user->getFullname()) .'</td>
-                                <td>'. htmlspecialchars($user->getRole()) .'</td>
+                                <td class="edit role">'. htmlspecialchars($user->getRole()) .'</td>
                                 <td>'. htmlspecialchars($user->getCreatedAt()) .'</td>
+                                <td><button>Upravit</button></td>
                             </tr>';
                     }
                 }
-            ?>
-            </tbody>
-        </table>
-    </section>
-    <section>
-
-    </section>
+?>
+                </tbody>
+            </table>
+        </section>
+        <section class="table-data">
+            <h2>Články</h2>
+            <p id="warning-display-articles">Zobrazení článků není podporované, kvůli množštví informací, v takto uzkem formátu, zkuste otočtit prosím zařízení na šířku, nebo použijte počítač.</p>
+            <table class="articles-table">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nadpis</th>
+                    <th>Podnadpis</th>
+                    <th>Obsah</th>
+                    <th>Obrázky</th>
+                    <th>Autor</th>
+                    <th>Datum zveřejnění</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                if (empty($articles)) {
+                    echo '<tr><td colspan="5">Žádné články nenalezeny</td></tr>';
+                } else {
+                    foreach ($articles as $article) {
+                        echo '<tr>
+                                <td>'. $article->getId() .'</td>
+                                <td>'. htmlspecialchars($article->getTitle()) .'</td>
+                                <td>'. htmlspecialchars($article->getSubtitle()) .'</td>
+                                <td>'. htmlspecialchars($article->getContent()) .'</td>
+                                <td>'. implode(',', $article->getImagePaths()) .'</td> <!--TODO: ADD IMAGE PREVIEW-->
+                                <td>'. $article->getAuthorId() .'</td>
+                                <td>'. $user->getCreatedAt() .'</td>
+                                <td><a href="'. UrlHelper::baseUrl('article/edit?id='. $article->getId()) .'"><button>Upravit</button></a></td>
+                            </tr>';
+                    }
+                }
+?>
+                </tbody>
+            </table>
+        </section>
+    </div>
+    <div class="overlay">
+        <div class="overlay-content">
+            <button class="overlay-close">X</button>
+            <h1></h1>
+            <p></p>
+        </div>
+    </div>
 </main>
+<script src="<?= UrlHelper::baseUrl('assets/js/tableDataEditing.js') ?>"></script>
---
+++ www/assets/js/tableDataEditing.js
@@ -0,0 +1,51 @@
+const tables = document.querySelectorAll('.articles-table');
+const overlay = document.querySelector('.overlay');
+const overlayContent = document.querySelector('.overlay-content');
+const overlayClose = document.querySelector('.overlay-close');
+
+
+// Opening & closing overlay
+function openOverlay(header, content) {
+    overlay.style.display = 'block';
+    overlay.querySelector('h1').textContent = header;
+    overlay.querySelector('p').innerHTML = content;
+
+    // Display images if there are any
+    if (header.includes('Obrázky')) {
+        let images = content.split(',');
+        images.forEach(image => {
+            let img = new Image();
+            img.src = image;
+            overlayContent.appendChild(img);
+        });
+
+        overlay.querySelector('p').innerHTML = content.split(',').join('<br>');
+    }
+}
+
+function closeOverlay() {
+    overlay.style.display = 'none';
+    overlay.querySelectorAll('img').forEach(img => img.remove()); // remove images from overlay
+}
+
+//
+
+
+// Event listeners
+overlayClose.addEventListener('click', closeOverlay);
+overlay.addEventListener('click', closeOverlay); // Close overlay on click outside of it
+overlayContent.addEventListener('click', (e) => e.stopPropagation()); // Prevent close on click inside overlay (e.g. on content/text for copying...)
+
+tables.forEach(table => {
+    // Get all rows
+    let rows = table.querySelectorAll('tr');
+
+    // For each row add "open overley with detailed content on click", except for the first row (header)
+    for (let i = 1; i < rows.length; i++) {
+        for (let j = 0; j < rows[i].children.length; j++) {
+            rows[i].children[j].addEventListener('click', () => {
+                openOverlay(rows[0].children[j].textContent, rows[i].children[j].textContent);
+            });
+        }
+    }
+});
