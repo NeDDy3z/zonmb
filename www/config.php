92859d214bb3c972e2461f0e78b2e25ab6ba69a6
Vaněk <erikvanek0@gmail.com>
2024-12-30 19:25:50 +0100

    fixed config

--- www/config.php
+++ www/config.php
@@ -56,15 +56,18 @@ ini_set('max_input_time', '60');            // Maximum request input processing 
 // Disable error reporting for development
 ini_set('display_errors', '0');              // Display runtime errors
 ini_set('display_startup_errors', '0');      // Display startup errors
-error_reporting(1);                      // Report all errors and warnings
+error_reporting(1);                             // Report all errors and warnings
 
-
-// Define default global constants
-define('BASE_URL', '');
-define('DEFAULT_PFP', 'assets/uploads/profile_images/_default.jpeg');
 
 // Load local configuration
 if (file_exists(ROOT . 'www/config.local.php')) {
     require ROOT . 'www/config.local.php';
-    return;
+}
+
+// Define default global constants
+if (!defined('BASE_URL')) {
+    define('BASE_URL', '');
+}
+if (!defined('DEFAULT_PFP')) {
+    define('DEFAULT_PFP', 'assets/uploads/profile_images/_default.jpeg');
 }
